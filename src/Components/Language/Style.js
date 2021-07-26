e5db4716355ee04fda730996becb6128fd74b935
mightyFZeus <bolarinolabisi36@gmail.com>
2021-07-26 17:52:11 +0100

    k

--- src/Components/Language/Style.js
+++ src/Components/Language/Style.js
@@ -3,13 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
     card:{
-      
        
         marginBottom: '3em',
-        [theme.breakpoints.down('sm')]: {
+        [theme.breakpoints.up('md')]: {
              marginRight:'1em',
-             textAlign:'center',
-              
           },
           "&:hover":{
             backgroundColor:'#ffffff',
