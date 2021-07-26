3e74c0272c896a611e48a2ec4dd3475a8d2290c1
mightyFZeus <bolarinolabisi36@gmail.com>
2021-07-26 17:37:17 +0100

    mobile align

--- src/Components/Language/Style.js
+++ src/Components/Language/Style.js
@@ -3,10 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   toolbar: theme.mixins.toolbar,
     card:{
+      
        
         marginBottom: '3em',
-        [theme.breakpoints.up('md')]: {
+        [theme.breakpoints.down('sm')]: {
              marginRight:'1em',
+             textAlign:'center',
+              
           },
           "&:hover":{
             backgroundColor:'#ffffff',
